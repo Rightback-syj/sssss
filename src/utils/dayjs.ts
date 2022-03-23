cd2c65a2c98fb3e42aa0bb8705bd0ca57672c6dc
Rightback-syj <80335609+Rightback-syj@users.noreply.github.com>
2022-03-23 12:00:01 +0800

    Update dayjs.ts
    修改日期

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -6,13 +6,13 @@ dayjs.extend(duration)
 dayjs.extend(LocalizedFormat)
 
 const WEEKS: { [key: number]: string } = {
-  1: '星期一',
-  2: '星期二',
-  3: '星期三',
-  4: '星期四',
-  5: '星期五',
-  6: '星期六',
-  0: '星期日',
+  1: '星期二',
+  2: '星期三',
+  3: '星期四',
+  4: '星期五',
+  5: '星期六',
+  6: '星期日',
+  0: '星期一',
 }
 
 export const weekToday = () => {
